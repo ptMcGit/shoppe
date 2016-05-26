e459167d2769edc500e2c11f6dc709a9c2472636
Zach H <zdhadgraft@gtcc.edu>
2016-05-26 15:40:16 -0400

    Passing transaction parser tests.

--- transaction_parser.rb
+++ transaction_parser.rb
@@ -1,2 +1,17 @@
+require "json"
+
 class TransactionParser
+
+  attr_reader :transaction
+
+    def initialize abs_filename
+      @contents = JSON.parse File.read abs_filename
+      @transaction = []
+  end
+
+    def parse!
+      @contents.each do |transaction|
+        @transaction.push transaction
+      end
+    end
 end
