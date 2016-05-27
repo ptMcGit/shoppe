7c882a2c97111585a957b12de99613c3fb9b70ac
Zach H <zdhadgraft@gtcc.edu>
2016-05-27 16:04:05 -0400

    @path is now an array; parse! now removes @contents after parsing.

--- transaction_parser.rb
+++ transaction_parser.rb
@@ -6,12 +6,18 @@ class TransactionParser
 
     def initialize abs_filename
       @contents = JSON.parse File.read abs_filename
+      @path = [abs_filename]
       @transaction = []
   end
 
     def parse!
+      #binding.pry
       @contents.each do |transaction|
         @transaction.push transaction
       end
+
+      # remove original data from the object
+
+      remove_instance_variable(:@contents)
     end
 end
